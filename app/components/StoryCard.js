bc9873b5b37d8ddd2ec9e0b6f3752d4758aefaf3
Vinu's Mac <Vineet.22566@sscbs.du.ac.in>
2024-08-18 21:19:54 +0530

    success story

--- app/components/StoryCard.js
+++ app/components/StoryCard.js
@@ -9,13 +9,13 @@ import Image from 'next/image';
 
 export default function StoryCard({title, description }) {
     return (
-        <Card className="flex h-[25vh] justify-center items-center">
+        <Card className="flex h-[24vh] justify-center items-center">
             <div className='w-1/3 flex justify-center items-center'>
-                <Image src="/avatar.jpg" width={40} height={40} className='w-[80%]' alt={title} />
+                <img src="https://cdn.pixabay.com/photo/2013/07/12/18/38/avatar-153605_1280.png" className='w-fit object-cover' alt={title} />
             </div>
             <CardContent className='w-2/3 h-[80%]'>
-                <CardTitle>{title}</CardTitle>
-                <CardDescription className="h-[100%] overflow-auto">
+                <CardTitle className="text-xl">{title}</CardTitle>
+                <CardDescription className="h-[100%] mt-2 overflow-auto">
                     {description}
                 </CardDescription>
             </CardContent>
