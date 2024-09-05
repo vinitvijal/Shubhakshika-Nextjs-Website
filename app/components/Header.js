5ad6bee38cd3a52674e08055328dd13f7dc51e38
7sumona02 <hawkingsumona8@gmail.com>
2024-09-05 15:14:34 +0530

    edits

--- app/components/Header.js
+++ app/components/Header.js
@@ -25,16 +25,15 @@ function Header() {
           <img className="toggle hidden" src="https://img.icons8.com/fluent-systems-regular/2x/close-window.png" width="48" height="48" />
         </button>
       </div>
-      <div className="toggle hidden w-full md:w-auto md:flex md:justify-evenly text-right text-bold mt-5 md:mt-0 border-t-2 border-black md:border-none p-5 bg-white md:bg-transparent">
-        <Link href="/" className="block md:inline-block text-black hover:text-yellow-400 px-3 py-3 border-b-2 border-black md:border-none">Home</Link>
-        <Link href='/#aboutus' className="block md:inline-block text-black hover:text-yellow-400 px-3 py-3 border-b-2 border-black md:border-none">About Us</Link>
-        <Link href='/#story' className="block md:inline-block text-black hover:text-yellow-400 px-3 py-3 border-b-2 border-black md:border-none">Stories</Link>
-        <Link href="/#projects" className="block md:inline-block text-black hover:text-yellow-400 px-3 py-3 border-b-2 border-black md:border-none">Our Projects</Link>
-        <Link href="/#sponsor" className="block md:inline-block text-black hover:text-yellow-400 px-3 py-3 border-b-2 border-black md:border-none">Sponsors</Link>
-        <Link href="/#events" className="block md:inline-block text-black hover:text-yellow-400 px-3 py-3 border-b-2 border-black md:border-none">Events</Link>
-        <Link href="/#contact" className="block md:inline-block text-black hover:text-yellow-400 px-3 py-3 border-b-2 border-black md:border-none">Contact</Link>
-        <Link href='/teams' className="block md:inline-block text-black hover:text-yellow-400 px-3 py-3 border-b-2 border-black md:border-none">Our Teams</Link>
-        <div className='mt-2 md:mt-0'><Link href="#" className="block md:inline-block w-full md:w-auto px-3 py-2 text-right bg-yellow-400 hover:bg-yellow-400/90 text-white rounded-lg font-semibold text-lg">Volunteer!</Link></div>
+      <div className="toggle hidden w-full md:w-auto md:flex md:justify-evenly text-right text-sm mt-5 md:mt-0 border-t-2 border-black md:border-none p-5 bg-white md:bg-transparent">
+        <Link href='/#aboutus' className="block md:inline-block text-black hover:text-yellow-400 px-4 py-3 border-b-2 border-black md:border-none">About Us</Link>
+        <Link href='/#story' className="block md:inline-block text-black hover:text-yellow-400 px-4 py-3 border-b-2 border-black md:border-none">Stories</Link>
+        <Link href="/#projects" className="block md:inline-block text-black hover:text-yellow-400 px-4 py-3 border-b-2 border-black md:border-none">Our Projects</Link>
+        <Link href="/#sponsor" className="block md:inline-block text-black hover:text-yellow-400 px-4 py-3 border-b-2 border-black md:border-none">Sponsors</Link>
+        <Link href="/#events" className="block md:inline-block text-black hover:text-yellow-400 px-4 py-3 border-b-2 border-black md:border-none">Events</Link>
+        <Link href="/#contact" className="block md:inline-block text-black hover:text-yellow-400 px-4 py-3 border-b-2 border-black md:border-none">Contact</Link>
+        <Link href='/teams' className="block md:inline-block text-black hover:text-yellow-400 px-4 py-3 border-b-2 border-black md:border-none">Our Team</Link>
+        <div className='mt-2 md:mt-1'><Link href="#" className="block md:inline-block w-full md:w-auto px-4 py-2 text-right bg-yellow-400 hover:bg-yellow-400/90 text-white rounded-lg font-semibold text-sm">Volunteer!</Link></div>
       </div>
     </nav>
     </div>
